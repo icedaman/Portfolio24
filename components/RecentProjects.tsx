d547e9a130f4d7ae70170e20c6fea46b171a9d57
icedaman <36274654+icedaman@users.noreply.github.com>
2024-07-10 15:30:09 +0100

    make projects responsive for 2k res

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
         A small selection of {' '}
         <span className='text-purple'>recent projects</span>
       </h1>
-      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+      <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10 3xl:gap-y-40'>
         {projects.map(({ id, title, des, img, iconLists, link })=> (
           <div key={id} className='sm:h-[41rem] lg:min-h-[32.5rem] h-[32rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
             <PinContainer title={link} href={link}>
