56a379010c41e6245c3ab37e37a82eeb4ede8123
icedaman <36274654+icedaman@users.noreply.github.com>
2024-07-11 03:45:53 +0100

    Footer created + bug fixes

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -6,7 +6,7 @@ import { CanvasRevealEffect } from "@/components/ui/CanvasRevealEffect";
 
 export const Approach = () => {
   return (
-    <section className="w-full py-20">
+    <section className="w-full py-20" id="approach">
       <h1 className="heading">
         My<span className="text-purple"> approach</span>
       </h1>
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -90,14 +90,12 @@ export const BentoGridItem = ({
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
               <div className="flex flex-col gap-3 lg:gap-8">
-                {['React.js', 'Next.js', 'TypeScript'].map((item)=>(
+                {['React.js', 'Next.js', 'TypeScript', 'React Native'].map((item)=>(
                   <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
                 ))}
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
               </div>
               <div className="flex flex-col gap-3 lg:gap-8">
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                {['Node.js', 'MongoDB', 'PHP'].map((item)=>(
+                {['Node.js', 'MongoDB', 'PHP', 'Liquid'].map((item)=>(
                   <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">{item}</span>
                 ))}
               </div>
